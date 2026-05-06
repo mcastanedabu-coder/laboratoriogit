4be4964c6fc3332a05df95092cd455ddcb0a47a5
Amperax <jgiraldomar@unal.edu.co>
2026-05-06 08:41:56 -0500

    Se añadio código nuevo para el input

--- lab.cpp
+++ lab.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
-
+#include<fstream>
+#include<sstream>
 using namespace std;
 
 int main(){
+	ifstream archivoEntrada("input/archivoEntrada.txt");
 	
-	return 0;
+	if (archivoEntrada.is_open()){
+		cout<<"Archivo abierto"<<endl;
+		string linea;
+		
+		while (getline(archivoEntrada, linea)){
+			
+		}
+	}
 }
