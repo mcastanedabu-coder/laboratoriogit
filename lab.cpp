f88cba9a41dc9612bca78d18f3608b7242ac6529
unknown <jasgonzalezma@unal.edu.co>
2026-05-09 16:42:42 -0500

    Añadidas lineas para escribir en un archivo

--- lab.cpp
+++ lab.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 int main(){
 	ifstream archivoEntrada("input/archivoEntrada.txt");
-	
+	ofstream archivoSalida("output/archivoSalida.txt");
 	if (archivoEntrada.is_open()){
 		cout<<"Archivo abierto"<<endl;
 		string linea;
 		
 		while (getline(archivoEntrada, linea)){
-			
+			archivoSalida<< linea;			
 		}
+		cout<<"Archivo copiado correctamente"<< endl;
 	}
 }
